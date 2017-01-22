3e0437f563027f37bb4dd8f18fd11a2a4477e4b1
Carsten Larsen <cs@innolan.dk>
2017-01-22 18:21:56 +0100

    Update documentation

--- doc/html/search/typedefs_5.js
+++ doc/html/search/typedefs_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['word',['word',['../memcpy_8c.html#aafc9a69535613ac225df2d6195518116',1,'memcpy.c']]]
+  ['word',['word',['../memcpy_8c.html#a19036394f9c80a08fc846c96f668711c',1,'memcpy.c']]]
 ];
