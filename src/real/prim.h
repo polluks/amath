12da4bbd29117c78f884b33efcbff0d2ab1fc3e7
Stefan <stefan.haubenthal@gmail.com>
2018-08-09 20:28:54 +0000

    missing operator

--- src/real/prim.h
+++ src/real/prim.h
@@ -106,7 +106,7 @@ int __kernel_rem_pio2(double* x, double* y, int e0, int nx, int prec, const int*
 /* Detect other BE CPUs */
 #if !defined(__IEEE_LITTLE_ENDIAN) && !defined(__IEEE_BIG_ENDIAN)
 #if defined(PPCCPU)      || defined(__PPC__) || \
-    defined(__powerpc__) || defined(__powerpc64__) \
+    defined(__powerpc__) || defined(__powerpc64__) || \
     defined(__mips__) || defined(__mips)
 #define __IEEE_BIG_ENDIAN
 #endif
